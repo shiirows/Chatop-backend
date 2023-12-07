9caf207d32e417d9ecb20123fb0a8f832d8cd47a
Mazoyer <mazoyeralexandre@hotmail.fr>
2023-12-07 12:07:52 +0100

    add new project maven for repository

---
+++ src/main/java/chatop/mazoyer/alexandre/projetThree/ProjetThreeApplication.java
@@ -0,0 +1,13 @@
+package chatop.mazoyer.alexandre.projetThree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetThreeApplication.class, args);
+	}
+
+}
