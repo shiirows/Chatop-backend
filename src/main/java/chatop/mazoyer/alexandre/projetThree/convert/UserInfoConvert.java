aa9ec2227abc409205b4a40a904b38ed66f5dc27
Mazoyer <mazoyeralexandre@hotmail.fr>
2023-12-07 15:29:37 +0100

    add new user service, repository, request, controller, response and add security jwt

---
+++ src/main/java/chatop/mazoyer/alexandre/projetThree/convert/UserInfoConvert.java
@@ -0,0 +1,30 @@
+package chatop.mazoyer.alexandre.projetThree.convert;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
+import chatop.mazoyer.alexandre.projetThree.DTO.UserInfoDto;
+import chatop.mazoyer.alexandre.projetThree.models.User;
+
+@Component
+public class UserInfoConvert {
+
+	public UserInfoDto entityToDto(User user) {
+
+		UserInfoDto dto = new UserInfoDto();
+
+		dto.setId(user.getId());
+		dto.setName(user.getName());
+		dto.setEmail(user.getEmail());
+		dto.setCreated_at(user.getCreated_at());
+		dto.setUpdated_at(user.getUpdated_at());
+		return dto;
+	}
+
+	public List<UserInfoDto> entityToDto(List<User> userInfo) {
+
+		return userInfo.stream().map(x -> entityToDto(x)).collect(Collectors.toList());
+
+	}
+
+}
